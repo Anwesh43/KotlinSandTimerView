0ef41ffe0a504f48811e2f214b29952f1478e0eb
Anwesh43 <anweshthecool0@gmail.com>
2018-01-15 01:09:56 +0530

    adding SandTimerView class

--- sandtimerview/src/main/java/ui/anwesome/com/sandtimerview/SandTimerView.kt
+++ sandtimerview/src/main/java/ui/anwesome/com/sandtimerview/SandTimerView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.sandtimerview
 /**
  * Created by anweshmishra on 15/01/18.
  */
+import android.view.*
+import android.graphics.*
+import android.content.*
+class SandTimerView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
