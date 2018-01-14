ff55d8f4f9ddfd9897e23b2232c649f6b26bcc3a
Anwesh43 <anweshthecool0@gmail.com>
2018-01-15 01:24:27 +0530

    adding SandTimer

--- sandtimerview/src/main/java/ui/anwesome/com/sandtimerview/SandTimerView.kt
+++ sandtimerview/src/main/java/ui/anwesome/com/sandtimerview/SandTimerView.kt
@@ -19,4 +19,39 @@ class SandTimerView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class SandTimer(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(180f)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.scale(1f,1f-2*i)
+                paint.style = Paint.Style.STROKE
+                paint.strokeWidth = size/12
+                paint.strokeCap = Paint.Cap.ROUND
+                val path = Path()
+                path.moveTo(0f,0f)
+                path.lineTo(-size/2, size/2)
+                path.lineTo(size/2, size/2)
+                path.lineTo(0f,0f)
+                canvas.drawPath(path,paint)
+                paint.style = Paint.Style.FILL
+                val path2 = Path()
+                path2.moveTo(0f,0f)
+                path2.lineTo(-size/2, size/2)
+                path2.lineTo(size/2, size/2)
+                path2.lineTo(0f,0f)
+                canvas.drawPath(path2,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
